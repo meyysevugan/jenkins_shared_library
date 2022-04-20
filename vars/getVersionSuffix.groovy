ff933ce7a5bafcd2e55e563c8f456c0bfd629db2
meyy sevugan <msevugan@onetrust.com>
2022-04-20 16:37:32 -0400

    .

--- vars/getVersionSuffix.groovy
+++ vars/getVersionSuffix.groovy
@@ -3,6 +3,6 @@ String getVersionSuffix(Map config){
     if (config.isReleaseCandidate) {
         return config.rcNumber
     } else {
-        return env.rcNumber + '+ci.' + env.BUILD_NUMBER
+        return config.rcNumber + '+ci.' + env.BUILD_NUMBER
     }
 }
