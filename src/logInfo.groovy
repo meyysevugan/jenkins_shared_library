2763788b5ff1a5671cae4e7147a304fd970c8306
meyy sevugan <msevugan@onetrust.com>
2022-04-21 11:09:07 -0400

    Adding loginfo class

---
+++ src/logInfo.groovy
@@ -0,0 +1,12 @@
+
+def info(String message){
+    echo "The log level is ${message}"
+}
+
+def warn(String message){
+ echo "The log level is ${message}"   
+}
+
+def debug(String message){
+    echo "The log level is ${message}"
+}
